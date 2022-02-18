d443c549e5746fe828f48e68d358ae33e6f4c747
Filip Grebowski <developerfilip@Developer-Filip.broadband>
2022-02-18 19:26:05 +0000

    Started projects to interact with Deepgram Speech-to-Text

---
+++ src/script.js
@@ -0,0 +1,59 @@
+const addLongNameToElement = (array, string) => {
+
+    if (array.includes(string, 1)) {
+        const currentIndex = array.indexOf("name") + 1;
+        const textValue = array.slice(currentIndex, array.length).join(" ");
+
+        return textValue.charAt(0).toUpperCase() + textValue.slice(1);
+    }
+}
+
+navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+    
+
+    // Remove this if you will be integrating with your mobile phone.
+    if (!MediaRecorder.isTypeSupported('audio/webm')) { 
+        return alert('Browser not supported.');
+    }
+
+    const mediaRecorder = new MediaRecorder(stream);
+
+    const socket = new WebSocket('wss://api.deepgram.com/v1/listen', [
+        'token',
+        'DEEPGRAM_API_KEY',
+    ]);
+
+    socket.onopen = () => {
+        console.log({ event: 'onopen' });
+
+        document.querySelector('#status').textContent = 'Connected.';
+
+        mediaRecorder.addEventListener('dataavailable', async (event) => {
+            if (event.data.size > 0 && socket.readyState == 1) {
+              socket.send(event.data);
+            }
+        });
+        mediaRecorder.start(250);
+    }
+    
+    socket.onmessage = message => {
+
+        const received = JSON.parse(message.data);
+        const transcript = received.channel.alternatives[0].transcript;
+
+        if (transcript && received.is_final) {
+
+            console.log(received);
+            document.querySelector('#text').textContent += transcript + ' ';
+        }
+    }
+    
+    socket.onclose = () => {
+        console.log({ event: 'onclose' });
+    }
+    
+    socket.onerror = error => {
+        console.log({ event: 'onerror', error });
+    }
+    
+});
